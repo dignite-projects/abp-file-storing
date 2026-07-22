83aaf35e23194808e72a91bda0a6718cf7079e3e
杜冠魁 <hello@dignite.com>
2026-07-22 23:07:24 +0900

    fix: align file explorer error localization
    Closes #26

--- file-explorer/src/Dignite.FileExplorer.Domain.Shared/Dignite/FileExplorer/FileExplorerErrorCodes.cs
+++ file-explorer/src/Dignite.FileExplorer.Domain.Shared/Dignite/FileExplorer/FileExplorerErrorCodes.cs
@@ -4,20 +4,20 @@ public static class FileExplorerErrorCodes
 {
     public static class Directories
     {
-        public const string DirectoryAlreadyExist = "FileExplorer.Directory:0001";
-        public const string DirectoryNotExist = "FileExplorer.Directory:0002";
-        public const string InvalidDirectoryName = "FileExplorer.Directory:0003";
-        public const string InvalidMove = "FileExplorer.Directory:0004";
-        public const string ForbidMovingToChild = "FileExplorer.Directory:0005";
-        public const string DirectoryNotEmpty = "FileExplorer.Directory:0006";
+        public const string DirectoryAlreadyExist = "Dignite.FileExplorer:Directory:0001";
+        public const string DirectoryNotExist = "Dignite.FileExplorer:Directory:0002";
+        public const string InvalidDirectoryName = "Dignite.FileExplorer:Directory:0003";
+        public const string InvalidMove = "Dignite.FileExplorer:Directory:0004";
+        public const string ForbidMovingToChild = "Dignite.FileExplorer:Directory:0005";
+        public const string DirectoryNotEmpty = "Dignite.FileExplorer:Directory:0006";
     }
     public static class Files
     {
-        public const string CellNameNotApplicable = "FileExplorer.File:0001";
-        public const string CellNameNotFound = "FileExplorer.File:0002";
-        public const string ImageSizeTooSmall = "FileExplorer.File:0003";
-        public const string ImageResizeFailure = "FileExplorer.File:0004";
-        public const string InvalidSorting = "FileExplorer.File:0005";
-        //public const string ImageCompressionFailure = "FileExplorer.File:0005";
+        public const string CellNameNotApplicable = "Dignite.FileExplorer:File:0001";
+        public const string CellNameNotFound = "Dignite.FileExplorer:File:0002";
+        public const string ImageSizeTooSmall = "Dignite.FileExplorer:File:0003";
+        public const string ImageResizeFailure = "Dignite.FileExplorer:File:0004";
+        public const string InvalidSorting = "Dignite.FileExplorer:File:0005";
+        //public const string ImageCompressionFailure = "Dignite.FileExplorer:File:0006";
     }
 }
