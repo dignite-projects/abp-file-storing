8517a06e212e52ec7baac39a95ec60a1ccec4521
杜冠魁 <hello@dignite.com>
2026-07-23 00:13:18 +0900

    perf: add file lookup indexes
    Add tenant/container lookup indexes for EF Core and MongoDB. Closes #16.

--- file-explorer/src/Dignite.FileExplorer.MongoDB/Dignite/FileExplorer/MongoDB/FileExplorerMongoDbContextExtensions.cs
+++ file-explorer/src/Dignite.FileExplorer.MongoDB/Dignite/FileExplorer/MongoDB/FileExplorerMongoDbContextExtensions.cs
@@ -1,5 +1,7 @@
 using Dignite.FileExplorer.Directories;
 using Dignite.FileExplorer.Files;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using Volo.Abp;
 using Volo.Abp.MongoDB;
 
@@ -20,6 +22,23 @@ public static class FileExplorerMongoDbContextExtensions
         builder.Entity<FileDescriptor>(x =>
         {
             x.CollectionName = FileExplorerDbProperties.DbTablePrefix + "FileDescriptors";
+            x.ConfigureIndexes(indexes =>
+            {
+                var keys = Builders<BsonDocument>.IndexKeys;
+
+                indexes.CreateOne(new CreateIndexModel<BsonDocument>(
+                    keys.Ascending(nameof(FileDescriptor.TenantId))
+                        .Ascending(nameof(FileDescriptor.ContainerName))
+                        .Ascending(nameof(FileDescriptor.Md5))));
+                indexes.CreateOne(new CreateIndexModel<BsonDocument>(
+                    keys.Ascending(nameof(FileDescriptor.TenantId))
+                        .Ascending(nameof(FileDescriptor.ContainerName))
+                        .Ascending(nameof(FileDescriptor.ReferBlobName))));
+                indexes.CreateOne(new CreateIndexModel<BsonDocument>(
+                    keys.Ascending(nameof(FileDescriptor.TenantId))
+                        .Ascending(nameof(FileDescriptor.ContainerName))
+                        .Ascending(nameof(FileDescriptor.EntityId))));
+            });
         });
     }
 }
